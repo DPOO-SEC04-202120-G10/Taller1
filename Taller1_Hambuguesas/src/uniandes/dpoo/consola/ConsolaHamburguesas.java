05e667286886d15305323d915b5cdff678d82442
Esteban Leiva <e.leivam@uniandes.edu.co>
2021-08-31 18:10:19 -0500

    Taller inicial

---
+++ Taller1_Hambuguesas/src/uniandes/dpoo/consola/ConsolaHamburguesas.java
@@ -0,0 +1,11 @@
+package uniandes.dpoo.consola;
+
+public class ConsolaHamburguesas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Inicio de ejecución de la aplicación");
+
+	}
+
+}
