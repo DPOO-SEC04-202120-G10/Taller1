7b23f687757f5771f04f42eb84eb9d1822b4c85d
tu usuario github <tu correo>
2021-08-31 19:36:00 -0500

    hgf

--- Taller1_Hambuguesas/src/uniandes/dpoo/modelo/ProductoMenu.java
+++ Taller1_Hambuguesas/src/uniandes/dpoo/modelo/ProductoMenu.java
@@ -1,5 +1,8 @@
 package uniandes.dpoo.modelo;
 
 public class ProductoMenu {
-
+	+ProductoMenu(String nombre, int precioBase)
+	+getNombre(): String
+	+getPrecio(): int
+	+generarTextoFactura(): String
 }
