f852c5d5bb2e1565312e5531226da5f8a0237c5e
tu usuario github <tu correo>
2021-08-31 19:11:52 -0500

    hola

--- Taller1_Hambuguesas/src/uniandes/dpoo/modelo/Restaurante.java
+++ Taller1_Hambuguesas/src/uniandes/dpoo/modelo/Restaurante.java
@@ -1,5 +1,40 @@
 package uniandes.dpoo.modelo;
 
 public class Restaurante {
+	public Restaurante()
+	{
+		
+	}
+public void iniciarPedido(String nombreCliente, String direcciónCliente)
+{
+	
+}
+public  void cerrarYGuardarPedido()
+{
+	
+}
+public getPedidoEnCurso() 
+{return Pedido
+		
+}
+public getMenuBase()
+{ return ArrayList&lt;Producto&gt
 
+}
+public void cargarInformacionRestaurante(File archivoIngredientes, File archivoMenu, File archivoCombos)
+{
+	
+}
+private void cargarIngredientes(File archivoIngredientes)
+{
+	
+}
+private void cargarMenu(File archivoMenu)
+{
+	
+}
+private void cargarCombos(File archivoCombos)
+{
+	
+}
 }
